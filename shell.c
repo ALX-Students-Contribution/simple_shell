370d8abd36d6448c596cd5f997c713e4c77a0b19
DestinedCodes <yiradesat@gmail.com>
2023-08-02 18:22:06 +0100

    stuck on path

--- shell.c
+++ shell.c
@@ -2,20 +2,57 @@
 
 int main(void)
 {
-	char *line, *commands[10];
+	char *line, **commands, **path, *path_val;
+
 	size_t n = 0;
+	int i;
+	pid_t cpid;
 
 	while(1)
 	{
+		i = 0;
+		commands = malloc(30);
+		path = malloc(50);
 		line = NULL;
+
 		printf("$ ");
 		getline(&line, &n, stdin);
 
-		commands[0] = line;
-		commands[1] = NULL;
+		line = strtok(line, "\n");
+		path_val = getenv("PATH");
 
-		execve(line, commands, NULL);
+		commands[i] = strtok(line, " ");
+		while(commands[i])
+		{
+			i++;
+			commands[i] = strtok(NULL, " ");
+		}
 
+		i = 0;
+		path[i] = malloc(100);
+		path[i] = strtok(path_val, ":");
+		while(path[i])
+		{
+			path[i] = strcat(path[i], "/ls");
+			i++;
+			path[i] = malloc(100);
+			path[i] = strtok(NULL, ":");
+		}
+
+		printf("%s\n", path[0]);
+
+		cpid = fork();
+		if (cpid == -1)
+			return (-1);
+		else if (cpid == 0)
+		{
+			execve(commands[0], commands, NULL);
+		}
+		else
+			wait(NULL);
+
+		free(path);
+		free(commands);
 		free(line);
 	}
 
--- shell.h
+++ shell.h
@@ -6,6 +6,11 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <string.h>
+#include <unistd.h>      
+#include <sys/types.h>
+#include <sys/wait.h>
+
+extern char **environ;
 
 #endif /* SHELL_H */
